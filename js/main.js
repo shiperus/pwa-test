ffa747ed7b600eb2b13268b207caf41009f57230
CJ <list.black32@yahoo.com>
2022-05-10 09:57:43 +0700

    ch

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
     .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   }
